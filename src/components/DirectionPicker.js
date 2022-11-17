b74348c9c5f75f58a883e4e80299d00e2123d7cf
jaysmith13 <janeasmith13@gmail.com>
2022-11-17 16:37:18 -0700

    Update DirectionPicker.js
    added arrows and directions

---
+++ src/components/DirectionPicker.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+const directionArrows = {
+    up: "↑",
+    leftup: "↖",
+    rightup: "↗",
+    left: "←",
+    center: "x",
+    right: "→",
+    rightdown: "↘",
+    leftdown: "↙",
+    down: "↓"
+    
+};
+
+const displayDirections = [
+    "up",
+    "leftup",
+    "rightup",
+    "left",
+    "center",
+    
+]
