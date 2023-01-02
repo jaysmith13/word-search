4051cb10008a97af17c1d02de8093cd4fa658541
jaysmith13 <janeasmith13@gmail.com>
2023-01-01 17:02:35 -0700

    Update DirectionPicker.js
    fixed bug

--- src/components/DirectionPicker.js
+++ src/components/DirectionPicker.js
@@ -1,34 +1,64 @@
-import React, {Component} from "react";
-
-const directionArrows = {
-    up: "↑",
-    leftup: "↖",
-    rightup: "↗",
-    left: "←",
-    center: "x",
-    right: "→",
-    rightdown: "↘",
-    leftdown: "↙",
-    down: "↓"
-    
-};
+import React, { Component } from "react";
 
 const displayDirections = [
-    "up",
-    "leftup",
-    "rightup",
-    "left",
-    "center",
-    "right",
-    "rightdown",
-    "leftdown",
-    "down"
-
+  "leftup",
+  "up",
+  "rightup",
+  "left",
+  "center",
+  "right",
+  "leftdown",
+  "down",
+  "rightdown"
 ];
 
-const Direction = ({directions, direction, onClick}) => {
-    let dirClass;
-    if (direction[direction]){
-        
-    }
+const directionArrows = {
+  leftup: "↖",
+  up: "↑",
+  rightup: "↗",
+  left: "←",
+  center: "x",
+  right: "→",
+  leftdown: "↙",
+  down: "↓",
+  rightdown: "↘"
+};
+
+const Direction = ({ directions, direction, onClick }) => {
+  let dirClass;
+  if (directions[direction]) {
+    dirClass = "arrow direction-on";
+  } else {
+    dirClass = "arrow direction-off";
+  }
+  if (direction === "center") {
+    return <div className="arrow"> </div>;
+  }
+  return (
+    <div className={dirClass} onClick={() => onClick(direction)}>
+      {directionArrows[direction]}
+    </div>
+  );
+};
+
+class DirectionPicker extends Component {
+  render() {
+    return (
+      <div className="direction-list">
+        <h3>Word directions</h3>
+        <div className="direction-arrows">
+          {displayDirections.map(direction => (
+            <Direction
+              directions={this.props.directions}
+              direction={direction}
+              onClick={this.props.onClick}
+              key={direction}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default DirectionPicker;
