36813db1ed90a4bf6efb28274e4853f65c2ebcc9
jaysmith13 <janeasmith13@gmail.com>
2023-01-01 17:09:46 -0700

    Update Lists.react.js
    adding grid consts

--- src/components/Lists.react.js
+++ src/components/Lists.react.js
@@ -4,3 +4,16 @@ import { Link } from 'react-router-dom';
 import { List, ListItem } from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 
+const style = {
+    margin: 20,
+    display: 'inline-block'
+};
+
+const GridItem = ({item}) => (
+    <div>
+        <Link to={item.id}>
+            <ListItem primaryText={item.name} />
+        </Link>
+    </div>
+);
+
